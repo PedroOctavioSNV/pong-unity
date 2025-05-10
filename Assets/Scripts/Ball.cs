7f6d9106933417f7675b3d88687597e65f801d0d
Pedro Octavio SNV <pedronovoa.21@gmail.com>
2025-05-09 21:28:40 -0300

    ScoreZones and ResetBall
    - Added ScoreZones objects and script.
    - Added ResetBall method to Ball script.
    - Added left and right variance to InitialPush function.
    - Modified Ball speed to 5.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,6 +5,8 @@ public class Ball : MonoBehaviour
     public new Rigidbody2D rigidbody2D;
     public float maxInitialAngle = 0.67f;
     public float moveSpeed = 1.0f;
+    public float startX = 0f;
+    public float maxStartY = 4f;
 
     private void Start()
     {
@@ -13,8 +15,27 @@ public class Ball : MonoBehaviour
 
     private void InitialPush()
     {
-        Vector2 direction = Vector2.left;
+        Vector2 direction = Random.value < 0.5f ? Vector2.left : Vector2.right;
+
         direction.y = Random.Range(-maxInitialAngle, maxInitialAngle);
         rigidbody2D.linearVelocity = direction * moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ScoreZone scoreZone = collision.GetComponent<ScoreZone>();
+        if (scoreZone)
+        {
+            Debug.Log("Scored a point!");
+            ResetBall();
+            InitialPush();
+        }
+    }
+
+    private void ResetBall()
+    {
+        float positionY = Random.Range(-maxStartY, maxStartY);
+        Vector2 position = new Vector2(startX, positionY);
+        transform.position = position;
     }
 }
