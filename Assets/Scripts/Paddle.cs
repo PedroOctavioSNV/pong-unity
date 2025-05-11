e646429adb27e391e2ade9c394b291f10f8cf25b
Pedro Octavio SNV <pedronovoa.21@gmail.com>
2025-05-11 20:05:54 -0300

    Player Input
    - Modified InputManager settings for Player 1 and 2.
    - Added ProcessInput and Move methods to Paddle script.
    - Modified Paddles Move Speed to 8.
    - Modified InputManager Gravity and Sensitivity to 1000 for Player 1 and 2.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public new Rigidbody2D rigidbody2D;
+    public float id;
+    public float moveSpeed = 2f;
+
+    public void Update()
     {
-        
+        float movement = ProcessInput();
+        Move(movement);
     }
 
-    // Update is called once per frame
-    void Update()
+    private float ProcessInput()
     {
-        
+        float movement = 0f;
+
+        switch (id)
+        {
+            case 1:
+                movement = Input.GetAxis("MovePlayer1");
+                break;
+            case 2:
+                movement = Input.GetAxis("MovePlayer2");
+                break;
+        }
+
+        return movement;
+    }
+
+    private void Move(float movement)
+    {
+        Vector2 velocity = rigidbody2D.linearVelocity
+    ;   velocity.y = moveSpeed * movement;
+        rigidbody2D.linearVelocity = velocity;
     }
 }
